359ff025c5732b80f53f74aa8b19a017b42c8234
Artem Maevskiy <amaevskij@gmail.com>
2023-12-13 20:07:14 +0800

    wrapped pes; adding checks; refactoring

--- pes_fingerprint/pes/__init__.py
+++ pes_fingerprint/pes/__init__.py
@@ -6,33 +6,56 @@ from ase import Atoms
 from ase.calculators.calculator import Calculator
 
 
-class PESManager:
+class PESCalculator:
     energy_validation_threshold: float = 0.2
-    assert_validation: bool = True
+    assert_energy_validation: bool = True
+    distance_validation_threshold: float = 0.5
+    assert_distance_validation: bool = True
+    radius_cutoff: float = 0.8
+    grid_size: Tuple[int, int, int] = (40, 40, 40)
 
     def __init__(
         self,
         source_atoms: Atoms, *,
-        grid_size: Tuple[int, int, int],
+        calculator: Union[Calculator, Callable],
         ids_of_interest: Optional[List[int]] = None,
         species_of_interest: Optional[str] = None,
+        **kwargs,
     ):
         assert (ids_of_interest is None) != (species_of_interest is None), (
             "Exactly one of the two arguments (ids_of_interest, species_of_interest) "
             "should be provided."
         )
-        self.atoms = source_atoms.copy()
-        self.grid_size = grid_size
+        self.source_atoms = source_atoms.copy()
 
         if species_of_interest is not None:
-            (self.soi_ids,) = np.where([el == species_of_interest for el in self.atoms.symbols])
+            (self.ids_of_interest,) = np.where([el == species_of_interest for el in self.source_atoms.symbols])
         elif ids_of_interest is not None:
-            self.soi_ids = np.array(ids_of_interest)
+            self.ids_of_interest = np.array(ids_of_interest)
         else:
             assert False
 
-        assert self.soi_ids.ndim == 1
-        assert len(self.soi_ids) > 0
+        assert self.ids_of_interest.ndim == 1
+        assert len(self.ids_of_interest) > 0
+
+        if isinstance(calculator, Calculator):
+            calculator = PESCalculator._wrap_calculator(calculator)
+        self.calculator = calculator
+
+        for k, v in kwargs.items():
+            assert hasattr(self, k), f"Bad argument: {str(k)}"
+            setattr(self, k, v)
+
+    def run(self):
+        self._calculate_pes()
+        self._find_minimum()
+
+        self.wrapped_imin, self.wrapped_pes = [], []
+        for imin, pes_i in zip(self.imin, self.pes):
+            (w_pes, w_imin) = PESCalculator._wrap_pes(pes_i, imin)
+            self.wrapped_imin.append(w_imin)
+            self.wrapped_pes.append(w_pes)
+
 
     @staticmethod
     def _generate_grid(cell: np.ndarray, size: Tuple[int, int, int]) -> np.ndarray:
@@ -60,16 +83,11 @@ class PESManager:
             return energies
         return _calculate
 
-    def calculate_pes(
-        self,
-        calculator: Union[Calculator, Callable],
-        cutoff: float = 0.8,
-    ) -> List[np.ndarray]:
-        if isinstance(calculator, Calculator):
-            calculator = PESManager._wrap_calculator(calculator)
-        grid = PESManager._generate_grid(self.atoms.cell.array, self.grid_size)
+    def _calculate_pes(self) -> None:
+        assert not hasattr(self, "pes"), "Seems like PES was already calculated"
+        grid = PESCalculator._generate_grid(self.source_atoms.cell.array, self.grid_size)
 
-        scaled_pos = self.atoms.get_scaled_positions()
+        scaled_pos = self.source_atoms.get_scaled_positions()
         assert (scaled_pos >= 0).all()
         assert (scaled_pos < 1).all()
 
@@ -77,37 +95,37 @@ class PESManager:
             np.sqrt(
                 (
                     (
-                        self.atoms[
-                            [i for i in range(len(self.atoms)) if i != i_mob]
+                        self.source_atoms[
+                            [i for i in range(len(self.source_atoms)) if i != i_mob]
                         ].repeat((3, 3, 3)).positions[:, None, None, None, :]
-                        - (grid + self.atoms.cell.array.sum(axis=0))[None]
+                        - (grid + self.source_atoms.cell.array.sum(axis=0))[None]
                     )**2
                 ).sum(axis=-1)
-            ).min(axis=0) >= cutoff
-            for i_mob in self.soi_ids
+            ).min(axis=0) >= self.radius_cutoff
+            for i_mob in self.ids_of_interest
         ]
 
         structs = []
-        for i_mob, mask in zip(tqdm(self.soi_ids, desc="Generating structures for calculation"), grid_valid):
+        for i_mob, mask in zip(tqdm(self.ids_of_interest, desc="Generating structures for calculation"), grid_valid):
             mobile_positions = grid[mask]
             assert mobile_positions.ndim == 2
             assert mobile_positions.shape[1] == 3
             structs.append([])
             for pos in mobile_positions:
-                structs[-1].append(self.atoms.copy())
+                structs[-1].append(self.source_atoms.copy())
                 structs[-1][-1].positions[i_mob] = pos
 
-        (self.base_energy,) = calculator([self.atoms.copy()])
-        pes = []
+        (self.base_energy,) = self.calculator([self.source_atoms.copy()])
+        self.pes = []
         for structs_subset, mask in zip(structs, grid_valid):
             energies = np.empty(dtype=float, shape=mask.shape)
-            energies[mask] = calculator(structs_subset)
+            energies[mask] = self.calculator(structs_subset)
             fill_value = energies[mask].max()
             energies[~mask] = fill_value
-            pes.append(energies)
-        self.pes = [pes_i.copy() for pes_i in pes]
+            self.pes.append(energies)
+
         failed_checks = []
-        for pes_i, i_mob in zip(self.pes, self.soi_ids):
+        for pes_i, i_mob in zip(self.pes, self.ids_of_interest):
             if pes_i.min() + self.energy_validation_threshold < self.base_energy:
                 failed_checks.append(
                     (pes_i.min(), i_mob)
@@ -121,11 +139,58 @@ class PESManager:
                     for emin, i_mob in failed_checks
                 )
             )
-            if self.assert_validation:
+            if self.assert_energy_validation:
                 assert False, msg
             else:
                 print(f"=== === === WARNING! === === ===")
                 print(msg)
                 print(f"=== === === ===  === === === ===")
 
-        return pes
+    def _find_minimum(self):
+        assert not hasattr(self, "imin"), "Seems like minima were already found"
+        self.imin = [
+            np.unravel_index(pes_i.argmin(), pes_i.shape)
+            for pes_i in self.pes
+        ]
+        np_imin = np.array(self.imin)
+        assert not ((np_imin == 0) | (np_imin == self.grid_size)).any(), "Minimum at the edge of the grid"
+        failed_checks = []
+        for imin, i_mob in zip(self.imin, self.ids_of_interest):
+            predicted = (np.array(imin) / np.array(self.grid_size)) @ self.source_atoms.cell.array
+            initial = self.source_atoms.positions[i_mob]
+            dist = np.linalg.norm(predicted - initial)
+            if dist > self.distance_validation_threshold:
+                failed_checks.append(
+                    (imin, i_mob, dist)
+                )
+        if len(failed_checks):
+            msg = (
+                f"Minima distance checks failed for the following species (distance threshold is {self.distance_validation_threshold:.3f} A):\n"
+                + "\n".join(
+                    f"  Ion #{i_mob}: distance is {dist:.3f} A, predicted i_min = {imin}, "
+                    f"base i_min = {tuple(np.round(self.source_atoms.get_scaled_positions()[i_mob] * self.grid_size, 3))}"
+                    for imin, i_mob, dist in failed_checks
+                )
+            )
+            if self.assert_distance_validation:
+                assert False, msg
+            else:
+                print(f"=== === === WARNING! === === ===")
+                print(msg)
+                print(f"=== === === ===  === === === ===")
+
+    @staticmethod
+    def _wrap_pes(
+        pes: np.ndarray,
+        location: Tuple[int, int, int],
+    ) -> Tuple[np.ndarray, Tuple[int, int, int]]:
+        """
+        Takes a PES 3d array and wraps it around twice, such that `location` is in the middle.
+        """
+        assert pes.ndim == 3
+
+        pes_3x3x3 = np.tile(pes, (3, 3, 3))
+        result = pes_3x3x3[tuple(slice(loc_i, loc_i + 2 * size_i + 1) for loc_i, size_i in zip(location, pes.shape))]
+        i_center = pes.shape
+        assert result[i_center] == pes[location]
+        return result, i_center
